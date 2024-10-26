125dd6062f856aa0830bad8fce31ddc3eee1ffc6
Jeesan57 <115409557+Jeesan57@users.noreply.github.com>
2024-10-27 01:12:02 +0600

    final update

--- pages/lookup.tsx
+++ pages/lookup.tsx
@@ -231,7 +231,7 @@ export default function Lookup() {
       </div>
 
       {(userData.can_view || isAdmin) && (
-        <div className="relative mt-4 flex items-center">
+        <div className="relative mt-2 flex items-center">
           <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
           <input
             type="text"
