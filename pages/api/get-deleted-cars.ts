34607038c5bf7607d81944413b49ca4acb748a59
Jeesan57 <115409557+Jeesan57@users.noreply.github.com>
2024-11-02 12:08:20 +0600

    u

--- pages/api/get-deleted-cars.ts
+++ pages/api/get-deleted-cars.ts
@@ -5,25 +5,21 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    // Only allow POST requests
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method Not Allowed' });
     }
 
     try {
-        // Query the 'Cars' table for deleted cars
         let supabase = createClient(req, res);
         const { data: deletedCars, error } = await supabase
             .from('Cars')
             .select('*')
             .eq('is_deleted', true);
 
-        // Handle any errors from the database query
         if (error) {
             return res.status(500).json({ error: 'Error retrieving deleted cars', details: error.message });
         }
 
-        // Respond with the list of deleted cars
         return res.status(200).json({ data: deletedCars });
         console.log(deletedCars);
     } catch (err) {
